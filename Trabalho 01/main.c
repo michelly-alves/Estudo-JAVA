8784fca8fc745326f9628776ac7313de8c159796
Michelly Alves <89709061+michelly-alves@users.noreply.github.com>
2022-08-24 09:50:22 -0300

    Update main.c

--- Trabalho 01/main.c
+++ Trabalho 01/main.c
@@ -11,16 +11,20 @@ typedef struct sapato Sapato;
 
 int main()
 {
+    //quantidade de sapatos e verifição
     int N;
     printf("Quantidade de sapatos (2<= N <=10000):\n");
     scanf("%d", &N);
+
     if (N %2 != 0 || N > 10000)
     {
     printf("Digite novamente a quantidade de sapatos:\n");
     scanf("%d", &N);
     }
+
     Sapato shoes[N];
 
+    //adquirindo dados dos sapatos junto com verifficações
     int i,j, cont = 0;
     for(i=0;i<N;i++)
     {
@@ -29,16 +33,17 @@ int main()
 
         if (shoes[i].M <= 30 || shoes[i].M >=60)
         {
-            printf("Numero do sapato e lado do cal�ado novamente:");
+            printf("Numero do sapato e lado do calçado novamente:");
             scanf("%d %s", &shoes[i].M, &shoes[i].C);
         }
         if(shoes[i].C != 'D' && shoes[i].C != 'E')
         {
-            printf("Numero do sapato e lado do cal�ado nocamente:");
+            printf("Numero do sapato e lado do calçado novamente:");
             scanf("%d %s", &shoes[i].M, &shoes[i].C);
         }
     }
 
+    ///juntando pares
     for(i=0;i<N;i++)
     {
         for(j=0;j<N;j++)
